701fbeff9e72951e41bdfde260f4ff59bf53bbce
leon-pash-ok <leonidovpavel84@gmail.com>
2017-06-05 17:14:05 +0300

    add log4j2

--- src/main/java/ua/leonpasha/main/Main.java
+++ src/main/java/ua/leonpasha/main/Main.java
@@ -1,14 +1,17 @@
 package ua.leonpasha.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.leonpasha.dao.SomeDao;
 
 /**
  * Created by HP on 04.06.2017.
  */
 public class Main {
-
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) throws Exception {
 
    /*     Server server = new Server();
@@ -17,8 +20,11 @@ public class Main {
         connector.setPort(8080);
         server.addConnector(connector);
 */
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(new String[] {"config.xml"});
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[] {"spring/springConfig.xml"});
+        SomeDao someDao = (SomeDao) applicationContext.getBean("SomeDao");
+        System.out.println("name = " + someDao.getName());
 
 
+        logger.debug("some text");
     }
 }
