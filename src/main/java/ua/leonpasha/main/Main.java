283e681517f567a2955d94adee39256c7bb9896e
leon-pash-ok <leonidovpavel84@gmail.com>
2017-06-05 18:17:33 +0300

    add from other project example jetty with servlet

--- src/main/java/ua/leonpasha/main/Main.java
+++ src/main/java/ua/leonpasha/main/Main.java
@@ -4,15 +4,41 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ua.leonpasha.dao.SomeDao;
-
+import ua.leonpasha.srvlt.SrvltCalculator;
+import org.eclipse.jetty.server.Handler;
 /**
  * Created by HP on 04.06.2017.
  */
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) throws Exception {
+        int port = 8088;
+        Server server = new Server(port);
+
+        ServletContextHandler context = new ServletContextHandler( ServletContextHandler.SESSIONS );
+        context.setContextPath( "/" );
+
+        // http://localhost:8080/func
+        context.addServlet(new ServletHolder( new SrvltCalculator() ),"/func");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { context });
+        server.setHandler(handlers);
+
+        try {
+            server.start();
+            System.out.println("Listening port : " + port );
+            server.join();
+
+        } catch (Exception e) {
+            System.out.println("Error.");
+            e.printStackTrace();
+        }
 
    /*     Server server = new Server();
 
@@ -20,11 +46,11 @@ public class Main {
         connector.setPort(8080);
         server.addConnector(connector);
 */
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[] {"spring/springConfig.xml"});
+        /*ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[] {"spring/springConfig.xml"});
         SomeDao someDao = (SomeDao) applicationContext.getBean("SomeDao");
-        System.out.println("name = " + someDao.getName());
+        System.out.println("name = " + someDao.getName());*/
 
 
-        logger.debug("some text");
+        //logger.debug("some text");
     }
 }
